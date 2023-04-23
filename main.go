98ec97842375fcc8351acdf11fefa081a9c5914d
goobric <goobric@gmail.com>
2023-04-23 21:21:38 +0800

    used placeholder data variables

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to the", conferenceName, "booking application!")
-	fmt.Println("There are", conferenceTickets, "tickets in total, and ", remainingTickets, " remaining to purchase.")
+	fmt.Printf("conferentTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	// placeholders in Go pkg.go.dev/fmt documentation
+	fmt.Printf("Welcome to the %v booking application!\n", conferenceName)
+	fmt.Printf("There are %v tickets in total, and %v remaining to purchase.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name and tickets
+
+	userName = "Nana"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets!\n", userName, userTickets)
 
 }
